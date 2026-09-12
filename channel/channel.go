b72e6fb1458a343a7a0b2a545c148d3fa6cebf83
alirezasm133 <alirezasm313@gmail.com>
2026-09-12 13:05:33 +0330

    practice 2:A few quantities,Done!

--- channel/channel.go
+++ channel/channel.go
@@ -1,21 +1,43 @@
+//practice 1
+
+// package main
+
+// import(
+// //	"sync"
+// 	"fmt"
+// )
+
+
+// func main(){
+// //	var wg sync.WaitGroup
+// 	ch:=make(chan int)
+// //	wg.Add(1)
+// 	go func(){
+// 		//defer wg.Done()
+// 		ch<-42
+// 	}()
+// 	fmt.Println(<-ch)
+// 	//wg.Wait()
+	
+	
+// }
+
+//practice2
 package main
-
-import(
-//	"sync"
-	"fmt"
-)
-
+import("fmt")
 
 func main(){
-//	var wg sync.WaitGroup
-	ch:=make(chan int)
-//	wg.Add(1)
+	ch:=make(chan string)
 	go func(){
-		//defer wg.Done()
-		ch<-42
+		ch<-"Go"
+		ch<-"is"
+		ch<-"awesome"
+		close(ch)
 	}()
-	fmt.Println(<-ch)
-	//wg.Wait()
-	
-	
+	//fmt.Println(<-ch)
+	// fmt.Println(<-ch)
+	// fmt.Println(<-ch)
+	for value:=range ch{
+		fmt.Println(value)
+	}
 }
